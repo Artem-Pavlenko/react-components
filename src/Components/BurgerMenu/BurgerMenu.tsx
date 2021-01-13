d78fbfc422eadb64f5e7ad481ca32ebcbe00ba7c
Artem <rozen19942810@gmail.com>
2021-01-13 14:37:46 +0200

    upd burger menu (v1)

--- src/Components/BurgerMenu/BurgerMenu.tsx
+++ src/Components/BurgerMenu/BurgerMenu.tsx
@@ -2,19 +2,13 @@ import React, {useRef, useState} from "react"
 import s from "./BurgerMenu.module.css"
 
 
-// import menuIcon from "../../assets/icon/menu.svg"
-// import closeIcon from "../../assets/icon/close.svg"
-
 export const BurgerMenu = () => {
 
     const [burger, setBurger] = useState<boolean>(false)
 
     const click = () => {
-        if (burger) {
-            setBurger(false)
-        } else if (!burger) {
-            setBurger(true)
-        }
+        burger && setBurger(false)
+        !burger && setBurger(true)
     }
 
     return (
