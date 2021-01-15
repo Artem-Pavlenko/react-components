51f78d85a10ded17ac7df5af718faed29c7e5e8a
Artem <rozen19942810@gmail.com>
2021-01-15 11:38:18 +0200

    add DragAndDrop for upload files

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {BurgerMenu} from "./Components/BurgerMenu/BurgerMenu";
 import {DragAndDrop_List} from "./Components/Drag And Drop/DragAndDrop_List/DragAndDrop_List";
 import {DragAndDrop2} from "./Components/Drag And Drop/DragAndDrop2/DragAndDrop2";
 import {DragAndDrop_trello} from "./Components/Drag And Drop/DragAndDrop_ difficult_version/DragAndDrop_trello";
+import {DragAndDrop_upload_file} from "./Components/Drag And Drop/DragAndDrop_upload_file/DragAndDrop_upload_file";
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
                     <li><Link to={'/dragAndDrop'}>Drag and drop</Link></li>
                     <li><Link to={'/dragAndDrop2'}>Drag and drop 2</Link></li>
                     <li><Link to={'/dragAndDrop-difficult_version'}>Drag and drop(difficult version)</Link></li>
+                    <li><Link to={'/dragAndDrop-upload_file'}>Drag and drop(upload file)</Link></li>
                 </ul>
             </div>
             <Switch>
@@ -26,6 +28,7 @@ function App() {
                 <Route path={'/dragAndDrop'} render={() => <DragAndDrop_List/>}/>
                 <Route path={'/dragAndDrop2'} render={() => <DragAndDrop2/>}/>
                 <Route path={'/dragAndDrop-difficult_version'} render={() => <DragAndDrop_trello/>}/>
+                <Route path={'/dragAndDrop-upload_file'} render={() => <DragAndDrop_upload_file/>}/>
             </Switch>
         </div>
     )
---
+++ src/Components/Drag And Drop/DragAndDrop_upload_file/DragAndDrop_upload_file.tsx
@@ -0,0 +1,55 @@
+import React, {useState, DragEvent} from 'react'
+import s from './DragAbdDrop_upload_file.module.css'
+
+export const DragAndDrop_upload_file = () => {
+
+    const [drag, setDrag] = useState<boolean>(false)
+
+    // function dragOverHandler(e: DragEvent<HTMLDivElement>) {
+    //     e.preventDefault()
+    //     setDrag(true)
+    // }
+
+    function dragStartHandler(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        setDrag(true)
+    }
+
+    function dragLeaveHandler(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        setDrag(false)
+    }
+
+    function dropHandler(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        let files = e.dataTransfer.files
+        // FileList type
+        console.log(files)
+        const formData = new FormData().append('file', files[0])
+        setDrag(false)
+    }
+
+    return (
+        <div className={s.mainBlock}>
+            {drag
+                ? <div
+                    className={s.dropArea}
+                    onDragStart={dragStartHandler}
+                    onDragOver={dragStartHandler}
+                    onDragLeave={dragLeaveHandler}
+                    onDrop={dropHandler}
+                >
+                    Отпустите файл, чтобы загрузить их
+                </div>
+                : <div
+                    className={s.dropAreaWithAutFile}
+                    onDragStart={dragStartHandler}
+                    onDragOver={dragStartHandler}
+                    onDragLeave={dragLeaveHandler}
+                >
+                    Перетащите файл, чтобы загрузить их
+                </div>
+            }
+        </div>
+    )
+}
